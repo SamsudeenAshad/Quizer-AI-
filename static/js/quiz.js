8d592fabf2e60a3ccc4e9a03738bd085aaad9f5f
Sim Shad <samsudeenashad@icloud.com>
2025-06-15 12:51:02 +0530

    update codes

---
+++ static/js/quiz.js
@@ -0,0 +1,13 @@
+// Quiz functionality JavaScript - minimal version
+
+// Utility functions
+function showElement(id) {
+    document.getElementById(id).classList.remove('d-none');
+}
+
+function hideElement(id) {
+    document.getElementById(id).classList.add('d-none');
+}
+
+// Main quiz functionality is handled in the HTML template
+// This file provides additional utilities if needed
